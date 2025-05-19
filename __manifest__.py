8d5d77b56ac793bf4f51f111f1e8317927050605
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-05-19 16:46:57 +0530

    Refactor module manifest and access rights; enhance match question templates and styles for improved UI and functionality

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,25 @@
 {
-    'name': 'Survey Match The Following',
-    'summary': 'Add "Match the Following" question type to Survey module',
-    'description': """
-        This module adds a new question type "Match the Following" to the Survey module.
-        It allows creating matching questions where users need to pair items from two columns.
-    """,
+    'name': 'Survey Match the Following Questions',
     'version': '17.0.1.0.0',
-    'category': 'Marketing/Surveys',
-    'author': 'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/survey',
-    'license': 'LGPL-3',
+    'category': 'Survey',
+    'summary': 'Add "Match the Following" question types to Odoo surveys',
+    'description': """
+This module adds a new question type "Match the Following" to Odoo surveys.
+Users can create match pairs where respondents need to match left options with right options.
+""",
     'depends': ['survey'],
     'data': [
         'security/ir.model.access.csv',
         'views/survey_question_views.xml',
         'views/survey_survey_views.xml',
-        'views/survey_templates.xml',
     ],
     'assets': {
         'web.assets_frontend': [
             'survey_match_the_following/static/src/js/match_render.js',
-            'survey_match_the_following/static/src/css/match_render.css',
-        ],
+            'survey_match_the_following/static/src/css/match_question.css',
+        ]
     },
     'installable': True,
     'application': False,
-    'auto_install': False,
+    'license': 'LGPL-3',
 }
