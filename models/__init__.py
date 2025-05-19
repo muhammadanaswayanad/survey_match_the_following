5ab81ee49a8b1ecc8b5ae46f4e836452d21e0d70
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-05-19 16:37:38 +0530

    Enhance "Match the Following" question type with UI improvements and logic updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
 from . import survey_question
 from . import survey_match_pair
 from . import survey_user_input_line
-from . import survey_new_model
--- static/src/js/match_render.js
+++ static/src/js/match_render.js
@@ -3,178 +3,148 @@ odoo.define('survey_match_the_following.match_render', function (require) {
 
 const core = require('web.core');
 const publicWidget = require('web.public.widget');
-const SurveyFormWidget = require('survey.form');
 const _t = core._t;
 
-publicWidget.registry.SurveyFormWidget.include({
-    /**
-     * @override
-     */
-    init: function () {
-        this._super.apply(this, arguments);
-        this.matchConnections = {};
-    },
+publicWidget.registry.SurveyMatchQuestion = publicWidget.Widget.extend({
+    selector: '.match_question_container',
 
+    events: {
+        'change .match-pair-select': '_onMatchPairChanged',
+    },
+    
     /**
      * @override
      */
     start: function () {
-        var def = this._super.apply(this, arguments);
-        this.$('.o_survey_match_question').each((index, element) => this._setupMatchQuestion($(element)));
-        return def;
+        var self = this;
+        return this._super.apply(this, arguments).then(function () {
+            self._initializeMatchQuestion();
+        });
     },
 
     /**
-     * Setup the match question interaction
-     * @param {jQuery} $matchQuestion
-     * @private
+     * Initialize the matching question UI
      */
-    _setupMatchQuestion: function ($matchQuestion) {
-        const questionId = $matchQuestion.data('question-id');
-        this.matchConnections[questionId] = {};
+    _initializeMatchQuestion: function () {
+        var questionId = this.$el.data('question-id');
         
-        // Set up the left options click handlers
-        $matchQuestion.find('.o_survey_match_left_item').on('click', (event) => {
-            const $target = $(event.currentTarget);
-            $matchQuestion.find('.o_survey_match_left_item').removeClass('active');
-            $target.addClass('active');
-        });
-
-        // Set up the right options click handlers
-        $matchQuestion.find('.o_survey_match_right_item').on('click', (event) => {
-            const $target = $(event.currentTarget);
-            const $activeLeft = $matchQuestion.find('.o_survey_match_left_item.active');
-
-            if ($activeLeft.length === 0) {
-                this._showMatchError(_t("Please select a left item first"));
-                return;
+        // Fetch the match pairs for this question
+        this._rpc({
+            model: 'survey.question',
+            method: 'read',
+            args: [[questionId], ['match_pair_ids']],
+        }).then(function (result) {
+            if (result && result.length) {
+                var matchPairIds = result[0].match_pair_ids;
+                
+                // Fetch the match pair details
+                this._rpc({
+                    model: 'survey.match.pair',
+                    method: 'read',
+                    args: [matchPairIds, ['left_option', 'right_option']],
+                }).then(function (pairs) {
+                    this._renderMatchPairs(questionId, pairs);
+                }.bind(this));
             }
-
-            // Get the ids
-            const leftId = $activeLeft.data('option-id');
-            const rightId = $target.data('option-id');
-
-            // Remove any previous connection for this left item
-            this._removeConnectionForLeft($matchQuestion, leftId);
-
-            // Create visual connection
-            this._createConnection($matchQuestion, leftId, rightId);
-
-            // Store the connection
-            this.matchConnections[questionId][leftId] = rightId;
-
-            // Update hidden field with JSON data
-            this._updateMatchInput(questionId);
-
-            // Reset active state
-            $activeLeft.removeClass('active');
-        });
-
-        // Setup the clear button
-        $matchQuestion.find('.o_survey_match_clear_btn').on('click', () => {
-            this._clearAllConnections($matchQuestion, questionId);
-        });
+        }.bind(this));
     },
 
     /**
-     * Remove connection for a left item
-     * @param {jQuery} $matchQuestion
-     * @param {string} leftId
-     * @private
+     * Render the match pairs UI
      */
-    _removeConnectionForLeft: function ($matchQuestion, leftId) {
-        const questionId = $matchQuestion.data('question-id');
-        const previousRightId = this.matchConnections[questionId][leftId];
+    _renderMatchPairs: function (questionId, pairs) {
+        var $container = this.$('.match-pairs-container');
+        $container.empty();
         
-        if (previousRightId) {
-            $matchQuestion.find(`.o_survey_match_connection[data-left-id="${leftId}"]`).remove();
-            delete this.matchConnections[questionId][leftId];
+        // Extract all right options for the dropdown
+        var rightOptions = pairs.map(function (pair) {
+            return pair.right_option;
+        });
+        
+        // Shuffle the right options for display
+        rightOptions = this._shuffleArray(rightOptions);
+        
+        // Create a row for each left option with a dropdown of right options
+        pairs.forEach(function (pair, index) {
+            var $row = $('<div/>', {
+                'class': 'row match-pair-row mb-2',
+                'data-left-option': pair.left_option
+            });
+            
+            var $leftCol = $('<div/>', {
+                'class': 'col-md-5 match-left-option',
+                'text': pair.left_option
+            });
+            
+            var $middleCol = $('<div/>', {
+                'class': 'col-md-2 text-center'
+            });
+            
+            var $rightCol = $('<div/>', {
+                'class': 'col-md-5'
+            });
+            
+            var $select = $('<select/>', {
+                'class': 'form-select match-pair-select',
+                'data-left-option': pair.left_option
+            });
+            
+            // Add an empty default option
+            $select.append($('<option/>', {
+                'value': '',
+                'text': _t('-- Select --')
+            }));
+            
+            // Add all right options
+            rightOptions.forEach(function (option) {
+                $select.append($('<option/>', {
+                    'value': option,
+                    'text': option
+                }));
+            });
+            
+            $rightCol.append($select);
+            $row.append($leftCol, $middleCol, $rightCol);
+            $container.append($row);
+        });
+    },
+    
+    /**
+     * Update the result when a match is selected
+     */
+    _onMatchPairChanged: function () {
+        var results = {};
+        var $selects = this.$('.match-pair-select');
+        
+        // Collect all selections
+        $selects.each(function () {
+            var $select = $(this);
+            var leftOption = $select.data('left-option');
+            var rightOption = $select.val();
+            
+            if (rightOption) {
+                results[leftOption] = rightOption;
+            }
+        });
+        
+        // Update the hidden input with JSON results
+        this.$('.match_result').val(JSON.stringify(results));
+    },
+    
+    /**
+     * Shuffle array elements (Fisher-Yates algorithm)
+     */
+    _shuffleArray: function (array) {
+        var shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
         }
-    },
-
-    /**
-     * Create a visual connection between left and right items
-     * @param {jQuery} $matchQuestion
-     * @param {string} leftId
-     * @param {string} rightId
-     * @private
-     */
-    _createConnection: function ($matchQuestion, leftId, rightId) {
-        const $leftItem = $matchQuestion.find(`.o_survey_match_left_item[data-option-id="${leftId}"]`);
-        const $rightItem = $matchQuestion.find(`.o_survey_match_right_item[data-option-id="${rightId}"]`);
-        
-        const $connection = $('<div>').addClass('o_survey_match_connection')
-            .attr('data-left-id', leftId)
-            .attr('data-right-id', rightId);
-            
-        $matchQuestion.find('.o_survey_match_connections').append($connection);
-            
-        // Highlight the connected items
-        $leftItem.addClass('connected');
-        $rightItem.addClass('connected');
-    },
-
-    /**
-     * Update the hidden input field with the current connections
-     * @param {number} questionId
-     * @private
-     */
-    _updateMatchInput: function (questionId) {
-        const $input = $(`input[name="question_${questionId}"]`);
-        $input.val(JSON.stringify(this.matchConnections[questionId]));
-    },
-
-    /**
-     * Clear all connections for a match question
-     * @param {jQuery} $matchQuestion
-     * @param {number} questionId
-     * @private
-     */
-    _clearAllConnections: function ($matchQuestion, questionId) {
-        $matchQuestion.find('.o_survey_match_connection').remove();
-        $matchQuestion.find('.o_survey_match_left_item, .o_survey_match_right_item').removeClass('connected active');
-        this.matchConnections[questionId] = {};
-        this._updateMatchInput(questionId);
-    },
-
-    /**
-     * Show an error message for match questions
-     * @param {string} message
-     * @private
-     */
-    _showMatchError: function (message) {
-        // Simple error display, can be enhanced later
-        alert(message);
-    },
-
-    /**
-     * @override
-     */
-    _validateForm: function () {
-        // Check match questions
-        const $matchQuestions = this.$('.o_survey_form .o_survey_match_question:not(.d-none)');
-        let errors = [];
-
-        $matchQuestions.each((index, element) => {
-            const $question = $(element);
-            const questionId = $question.data('question-id');
-            const isRequired = $question.data('required');
-            const connections = Object.keys(this.matchConnections[questionId] || {}).length;
-            const totalLeftOptions = $question.find('.o_survey_match_left_item').length;
-            
-            if (isRequired && connections < totalLeftOptions) {
-                errors.push({
-                    $el: $question,
-                    message: _t("Please match all items")
-                });
-            }
-        });
-
-        // Combine with other validations
-        return this._super.apply(this, arguments).then((baseErrors) => {
-            return baseErrors.concat(errors);
-        });
+        return shuffled;
     }
 });
 
+return {
+    SurveyMatchQuestion: publicWidget.registry.SurveyMatchQuestion
+};
 });
