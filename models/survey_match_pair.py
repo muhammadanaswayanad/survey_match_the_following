ed06d02df8315ba0a62bb91e1b3faf3da72abc96
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-05-19 16:48:10 +0530

    Remove unique constraint on left options in SurveyMatchPair model and adjust XML for match question result template

--- models/survey_match_pair.py
+++ models/survey_match_pair.py
@@ -10,9 +10,3 @@ class SurveyMatchPair(models.Model):
     question_id = fields.Many2one('survey.question', string='Question', ondelete='cascade', required=True)
     left_option = fields.Char('Left Option', required=True, translate=True)
     right_option = fields.Char('Right Option', required=True, translate=True)
-    
-    _sql_constraints = [
-        ('unique_left_option_per_question', 
-         'UNIQUE(question_id, left_option)', 
-         'Left options must be unique per question'),
-    ]
